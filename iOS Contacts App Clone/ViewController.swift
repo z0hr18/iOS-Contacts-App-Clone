70f2c4fb92ec99b3f50a01804a5035b9108b6f59
Zohra <guliyevaazohra@gmail.com>
2025-07-03 16:57:04 +0400

    Create the project

---
+++ iOS Contacts App Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Contacts App Clone
+//
+//  Created by Zohra Guliyeva on 7/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
